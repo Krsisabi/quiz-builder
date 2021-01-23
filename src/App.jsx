e5b4dec893478e96573ef15633ebd20f1000cd26
TimYus <timboyus@gmail.com>
2021-01-23 16:22:07 +0300

    add Quiz component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Layout from "./hoc/Layout/Layout"
+import Quiz from "./containers/Quiz/Quiz"
 
 function App() {
 	return (
 		<div className="App">
 			<Layout>
-				<div style={{ width: 400, border: '1px solid black' }}>
-					<h1>Layout works</h1>
-				</div>
+				<Quiz />
 			</Layout>
 		</div>
 	);
