cedbae6447b3f2cf1266db3234dfc128988b54d2
TimYus <timboyus@gmail.com>
2021-04-09 18:59:41 +0300

    add Drawer

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './Layout.module.scss'
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
 import { useState } from 'react'
 
 const Layout = props => {
@@ -13,6 +14,10 @@ const Layout = props => {
 	
 	return (
 		<div className={classes.Layout}>
+
+			<Drawer
+				isOpen={menu}
+			/>
 			
 			<MenuToggle
 				onToggle={toggleMenuHandler}
