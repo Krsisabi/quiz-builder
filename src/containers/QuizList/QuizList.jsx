d196e7086aebe41d4b61c7b9324a504c54588939
TimYus <timboyus@gmail.com>
2021-04-11 22:42:09 +0300

    QuizList stylizing

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import classes from './QuizList.module.scss'
+import { NavLink } from 'react-router-dom'
 
 export default class QuizList extends Component {
+
+	renderQuizzes() {
+		return [1, 2, 3].map((quiz, index) => {
+			return (
+				<li
+					key={index}
+				>
+					<NavLink to={`/quiz/${quiz}`}>
+						Test {quiz}
+					</NavLink>
+				</li>
+			)
+		})
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>Quiz List</h1>
+			<div className={classes.QuizList}>
+				<div>
+					<h1>Quiz List</h1>
+					<ul>
+						{this.renderQuizzes()}
+					</ul>
+				</div>
 			</div>
 		)
 	}
