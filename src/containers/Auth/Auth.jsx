622b56bec1a50c6d21be2e088e30f8b0eb96cba6
TimYus <timboyus@gmail.com>
2021-04-13 10:17:16 +0300

    add auth form

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
+import classes from './Auth.module.scss'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+
+	loginHandler = () => {
+		
+	}
+
+	registerHandler = () => {
+		
+	}
+
+	submitHandler = event => {
+		
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>Auth</h1>
+			<div className={classes.Auth}>
+				<div>
+					<h1>Sign up</h1>
+
+					<form onSubmit={this.submitHandler} className={classes.AuthForm}>
+						<input type="text" />
+						<input type="text" />
+
+						<Button
+							type="success"
+							onClick={this.loginHandler}
+						>
+							Sign in
+						</Button>
+
+						<Button
+							type="primary"
+							onClick={this.registerHandler}
+						>
+							Sign up
+						</Button>
+					</form>
+				</div>
 			</div>
 		)
 	}
