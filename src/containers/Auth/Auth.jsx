ae527bf34d5c17fbf7e6850bb926865cfb4e67b6
TimYus <timboyus@gmail.com>
2021-04-19 04:25:49 +0300

    add Input component

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import classes from './Auth.module.scss'
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
 
@@ -23,8 +24,14 @@ export default class Auth extends Component {
 					<h1>Sign up</h1>
 
 					<form onSubmit={this.submitHandler} className={classes.AuthForm}>
-						<input type="text" />
-						<input type="text" />
+
+						<Input
+							label="Email"
+						/>
+						<Input
+							label="Password"
+							errorMessage='TEST'
+						/>
 
 						<Button
 							type="success"
