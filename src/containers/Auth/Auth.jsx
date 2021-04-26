4f2ee28b5b12f74dfc1b7425378a695e7cecb00c
TimYus <timboyus@gmail.com>
2021-04-26 23:02:35 +0300

    add inputs validation

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -3,7 +3,41 @@ import classes from './Auth.module.scss'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 
+function validateEmail(email) {
+	const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return re.test(String(email).toLowerCase());
+}
+
 export default class Auth extends Component {
+
+	state = {
+		formControls: {
+			email: {
+				value: '',
+				type: 'email',
+				label: 'Email',
+				errorMessage: 'Email is incorrect',
+				valid: false,
+				touched: false,
+				validation: {
+					required: true,
+					email: true
+				}
+			},
+			password: {
+				value: '',
+				type: 'password',
+				label: 'Password',
+				errorMessage: 'Password is incorrect',
+				valid: false,
+				touched: false,
+				validation: {
+					required: true,
+					minLength: 6
+				}
+			}
+		}
+	}
 
 	loginHandler = () => {
 		
@@ -17,6 +51,63 @@ export default class Auth extends Component {
 		
 	}
 
+	validateControl(value, validation) {
+		if (!validation) {
+			return true
+		}
+
+		let isValid = true
+
+		if (validation.required) {
+			isValid = value.trim() !== '' && isValid
+		}
+
+		if (validation.email) {
+			isValid = validateEmail(value) && isValid
+		}
+
+		if (validation.minLength) {
+			isValid = value.trim().length >= validation.minLength && isValid
+		}
+
+		return isValid
+	}
+
+	onChangeHandler = (event, controlName) => {
+		const formControls = { ...this.state.formControls }
+		const control = { ...formControls[controlName] }
+
+		control.value = event.target.value
+		control.touched = true
+		control.valid = this.validateControl(control.value, control.validation)
+
+		formControls[controlName] = control
+
+		this.setState({
+			formControls
+		})
+	}
+
+	renderInputs() {
+		return Object.keys(this.state.formControls).map((controlName, index) => {
+			const control = this.state.formControls[controlName]
+			return (
+				<Input
+					key={controlName + index}
+					type={control.type}
+					value={control.value}
+					label={control.label}
+					errorMessage={control.errorMessage}
+					valid={control.valid}
+					touched={control.touched}
+					validation={control.validation}
+					shouldValidate={!!control.validation}
+					onChange={event => this.onChangeHandler(event, controlName)}
+				/>
+			)
+		})
+	}
+
 	render() {
 		return (
 			<div className={classes.Auth}>
@@ -25,13 +116,7 @@ export default class Auth extends Component {
 
 					<form onSubmit={this.submitHandler} className={classes.AuthForm}>
 
-						<Input
-							label="Email"
-						/>
-						<Input
-							label="Password"
-							errorMessage='TEST'
-						/>
+						{this.renderInputs()}
 
 						<Button
 							type="success"
