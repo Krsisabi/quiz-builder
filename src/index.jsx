c0553a41227562f948fe88b51d2bcd18a9bb7189
TimYus <timboyus@gmail.com>
2021-05-16 19:01:05 +0300

    setup redux

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import {compose, createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(
+		applyMiddleware(thunk)
+	)
+)
 
 const app = (
-	<BrowserRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
